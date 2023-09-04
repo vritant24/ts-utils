0637bcff33cbd0711bf8991ced697d3581180d41
vritant <vritoku@gmail.com>
2023-09-04 13:11:25 -0700

    add test configs

--- packages/async-action-pump/vitest.config.js
+++ packages/async-action-pump/vitest.config.js
@@ -1,9 +1,14 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import { f } from 'vitest/dist/types-3c7dbfa5'
 
 export default defineConfig({
     test: {
         include: ['*.test.ts'],
         root: './src/tests',
+        passWithNoTests: false,
+        reporters: [
+            'verbose'
+        ],
     },
 })
